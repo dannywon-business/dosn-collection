53eea787e9e886b2fa45cdeb1b9169ac6200fb19
원승대 <wondada91@gmail.com>
2024-08-10 15:59:14 +0900

    백앤드 서버 api주소 하드코딩

--- actions/dosn.action.ts
+++ actions/dosn.action.ts
@@ -7,7 +7,7 @@ import { revalidatePath } from 'next/cache';
 export const getAllProducts = async (authorName: string) => {
   const path = '/dosn/get-products';
 
-  const allProducts = await fetch(`${MGT_API_BASE_URI}${path}?authorName=${authorName}`, {
+  const allProducts = await fetch(`https://api-asia.mgt.gg/v1${path}?authorName=${authorName}`, {
     method: "GET",
     headers: { apikey: "PfmoiKctj04qpw93XCTug6bln81HhYrN" },
     next: { revalidate: 0 }
@@ -20,7 +20,7 @@ export const getAllProducts = async (authorName: string) => {
 export const getBestProducts = async (authorName: string, limit: number) => {
   const path = '/dosn/best-products';
 
-  const bestProducts = await fetch(`${MGT_API_BASE_URI}${path}?authorName=${authorName}&limit=${limit}`, {
+  const bestProducts = await fetch(`https://api-asia.mgt.gg/v1${path}?authorName=${authorName}&limit=${limit}`, {
     method: "GET",
     headers: { apikey: "PfmoiKctj04qpw93XCTug6bln81HhYrN" },
     next: { revalidate: 0 }
@@ -35,7 +35,7 @@ export const scoreProduct = async (prodIdx: number, score: number) => {
   const path = '/dosn/score-product';
   console.log('scoreProduct 함수 실행')
 
-  const updateResult = await fetch(`${MGT_API_BASE_URI}${path}`, {
+  const updateResult = await fetch(`https://api-asia.mgt.gg/v1${path}`, {
     method: "POST",
     mode: "cors",
     body: JSON.stringify({prodIdx, score}),
